0ed7270aa3ee785ee535e559465430fa4ec6d630
Matt Terry <mrterry@nextdoor.com>
2018-08-01 09:31:35 -0700

    can run tests

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ setup(
     name='conda_lockfile',
     version='0.1',
     packages=['conda_lockfile'],
-    install_requires=[
-        'PyYAML',
-    ],
-    entry_points={
-        'console_scripts': ['conda-lockfile = conda_lockfile.cli:main'],
-    },
+    install_requires=['PyYAML'],
+    test_requires=['pytest'],
+    entry_points={'console_scripts': ['conda-lockfile = conda_lockfile.cli:main']},
     include_package_data=True,
     package_dir={'mypkg': 'conda_lockfile'},
     package_data={'conda_lockfile': ['builder/Dockerfile', 'builder/build_lockfile.sh']},
