6641ad8f52c58a9ffa8788bf74eb85daa5d775fb
Matt Terry <mrterry@nextdoor.com>
2018-08-01 10:11:38 -0700

    pytest integration

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=['conda_lockfile'],
     install_requires=['PyYAML'],
+    setup_requires=['pytest-runner']
     tests_require=['pytest'],
     entry_points={'console_scripts': ['conda-lockfile = conda_lockfile.cli:main']},
     include_package_data=True,
