871930c4faad7d80a199d21b912b9bca00c97375
Matt Terry <mrterry@nextdoor.com>
2018-08-01 10:07:58 -0700

    still unable to spell

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=['conda_lockfile'],
     install_requires=['PyYAML'],
-    tests_requires=['pytest'],
+    tests_require=['pytest'],
     entry_points={'console_scripts': ['conda-lockfile = conda_lockfile.cli:main']},
     include_package_data=True,
     package_dir={'mypkg': 'conda_lockfile'},
