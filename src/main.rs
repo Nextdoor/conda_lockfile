5ec5a58f03e74c1a1611f7f0a721ef3c36a6afb8
Matt Terry <mrterry@nextdoor.com>
2020-08-05 12:04:07 -0700

    Make more verbose output (#15)
    * rename variable
    
    * More verbose output

--- src/main.rs
+++ src/main.rs
@@ -206,9 +206,9 @@ fn lockfile_is_up_to_date(lockfile_path: &str, env_hash: &str) -> bool {
     return false;
 }
 
-fn run_command(path: &str, args: &[&str]) -> ioResult<Output> {
-    info!("{}, {:?}", path, args);
-    let output = Command::new(path).args(args).output();
+fn run_command(executable: &str, args: &[&str]) -> ioResult<Output> {
+    info!("{}, {:?}", executable, args);
+    let output = Command::new(executable).args(args).output();
     match output {
         Ok(ok) => Ok(ok),
         Err(err) => {
@@ -486,7 +486,14 @@ fn handle_create(matches: &ArgMatches) -> Result<()> {
     // Copy lockfile to constructed env
     let mut embeded_lockfile = conda_prefix(&env_name)?;
     embeded_lockfile.push("deps.lock.yml");
-    copy(lockfile_path, embeded_lockfile)?;
+    let res = copy(&lockfile_path, embeded_lockfile);
+    match res {
+        Ok(status) => status,
+        Err(_) => {
+            let msg = format!("Unable to copy {} to ", lockfile_path);
+            return Err(ioError::new(ioErrorKind::Other, msg).into());
+        }
+    };
     Ok(())
 }
 
