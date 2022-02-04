8aae00964d7015142e84bc8aac3076971709e7ba
Richard Huang <rhuang@nextdoor.com>
2022-02-04 12:17:47 -0800

    Refactor

--- ci/script.sh
+++ ci/script.sh
@@ -5,11 +5,7 @@ set -e
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
-eval `ssh-agent -s`
-ssh-add
-
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
-
 cargo test
 
 export PATH="$HOME/miniconda/bin:$PATH"
