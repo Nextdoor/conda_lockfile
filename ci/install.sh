24750c3c9952c106e5908817c3bda7c4dcae8377
Richard Huang <rhuang@nextdoor.com>
2022-02-04 09:25:41 -0800

    bump miniconda

--- ci/install.sh
+++ ci/install.sh
@@ -3,19 +3,15 @@
 set -e
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-MacOSX-x86_64.sh"
+    URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.10.3-MacOSX-x86_64.sh"
 else
-    URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh"
+    URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.10.3-Linux-x86_64.sh"
 fi
 wget $URL -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-
-# `conda update -q conda` removes libffi.6.dylib and replaces it with libffi.7.dylib and libffi.8.dylib
-# libffi.6.dylib is required for `conda install`. https://github.com/conda/conda/issues/9038.
-ln -s $HOME/miniconda/lib/libffi.7.dylib $HOME/miniconda/lib/libffi.6.dylib
 
 # Useful for debugging any issues with conda
 conda info -a
