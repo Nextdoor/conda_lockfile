9b0699ac411e7c2f6751b5be3d7efdfa1eb4a0a3
Richard Huang <rhuang@nextdoor.com>
2022-02-03 18:52:37 -0800

    print info

--- ci/install.sh
+++ ci/install.sh
@@ -11,9 +11,16 @@ wget $URL -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 ls $HOME/miniconda/lib/
 export PATH="$HOME/miniconda/bin:$PATH"
+echo "here1"
 conda config --set always_yes yes --set changeps1 no
+ls $HOME/miniconda/lib/
+echo "here2"
 conda update -q conda
+ls $HOME/miniconda/lib/
+echo "here3"
 # Useful for debugging any issues with conda
 conda info -a
-
+ls $HOME/miniconda/lib/
+echo "here4"
 conda install -q conda-build anaconda-client
+ls $HOME/miniconda/lib/
