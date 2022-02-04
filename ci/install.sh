b45a7290f44f56ee7274cf95c864e27e9e3421cc
Richard Huang <rhuang@nextdoor.com>
2022-02-04 11:18:46 -0800

    cleanup

--- ci/install.sh
+++ ci/install.sh
@@ -10,15 +10,8 @@ fi
 wget $URL -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-echo "here conda config"
-ls $HOME/miniconda/lib/
 conda config --set always_yes yes --set changeps1 no
-echo "here conda update"
-ls $HOME/miniconda/lib/
 conda update -q conda
-
-echo "after conda update"
-ls $HOME/miniconda/lib/
 
 # Useful for debugging any issues with conda
 conda info -a
