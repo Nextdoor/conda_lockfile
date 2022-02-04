9256406818f21e67cc953fdd8c0014bee0b3306b
Richard Huang <rhuang@nextdoor.com>
2022-02-04 10:11:11 -0800

    print info

--- ci/install.sh
+++ ci/install.sh
@@ -10,8 +10,15 @@ fi
 wget $URL -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
+echo "here conda config"
+ls $HOME/miniconda/lib/
 conda config --set always_yes yes --set changeps1 no
+echo "here conda update"
+ls $HOME/miniconda/lib/
 conda update -q conda
+
+echo "after conda update"
+ls $HOME/miniconda/lib/
 
 # `conda update -q conda` removes libffi.6.dylib and replaces it with libffi.7.dylib and libffi.8.dylib
 # libffi.6.dylib is required for `conda install`. https://github.com/conda/conda/issues/9038.
