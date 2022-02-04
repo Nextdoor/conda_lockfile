f3625997c9fe608d7f4259f5301e9d75cbb1423f
Richard Huang <rhuang@nextdoor.com>
2022-02-03 19:24:21 -0800

    Debug build (#21)
    * symlink libffi after conda update
    
    Co-authored-by: Matt Terry <mrterry@nextdoor.com>

--- ci/install.sh
+++ ci/install.sh
@@ -12,6 +12,11 @@ bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
+
+# `conda update -q conda` removes libffi.6.dylib and replaces it with libffi.7.dylib and libffi.8.dylib
+# libffi.6.dylib is required for `conda install`. https://github.com/conda/conda/issues/9038.
+ln -s $HOME/miniconda/lib/libffi.7.dylib $HOME/miniconda/lib/libffi.6.dylib
+
 # Useful for debugging any issues with conda
 conda info -a
 
