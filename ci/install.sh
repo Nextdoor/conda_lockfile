91ca7260a0286b83bc33ebad2895a0b60cda01dc
Richard Huang <rhuang@nextdoor.com>
2022-02-03 18:36:28 -0800

    ls miniconda3/lib

--- ci/install.sh
+++ ci/install.sh
@@ -4,7 +4,6 @@ set -e
 
 if [[ "$(uname)" == "Darwin" ]]; then
     URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-MacOSX-x86_64.sh"
-    brew install libffi
 else
     URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh"
 fi
