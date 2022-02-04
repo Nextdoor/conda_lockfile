20ca7ccdf64ecdfeafc116416e94fd93eeb60e5c
Richard Huang <rhuang@nextdoor.com>
2022-02-03 16:55:01 -0800

    install libffi

--- ci/install.sh
+++ ci/install.sh
@@ -4,12 +4,13 @@ set -e
 
 if [[ "$(uname)" == "Darwin" ]]; then
     URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-MacOSX-x86_64.sh"
+    brew install libffi
 else
     URL="https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh"
 fi
 wget $URL -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
-ln -s $HOME/miniconda/lib/libffi.7.dylib $HOME/miniconda/lib/libffi.6.dylib
+ls $HOME/miniconda/lib/
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
