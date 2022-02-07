82c12c148f3146f979742542f3e984e5afdc6e7a
Richard Huang <rhuang@nextdoor.com>
2022-02-07 13:21:44 -0800

    debug ssh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,6 @@ set -e
 export PATH="$HOME/miniconda/bin:$PATH"
 
 cd recipe
-conda build .
+CARGO_NET_GIT_FETCH_WITH_CLI=true conda build .
 conda build . --output | xargs anaconda -t $ANACONDA_ORG_TOKEN upload
 
--- ci/script.sh
+++ ci/script.sh
@@ -6,9 +6,12 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
-cargo test
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo test
+
+eval `ssh-agent -s`
+ssh-add
 
 export PATH="$HOME/miniconda/bin:$PATH"
 cd recipe
-conda build .
+CARGO_NET_GIT_FETCH_WITH_CLI=true conda build .
 conda build . --output
