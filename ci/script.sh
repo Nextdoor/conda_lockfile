03ce054c93c557c6f1ef0fdfc9180a319f7dda30
Richard Huang <rhuang@nextdoor.com>
2022-02-09 16:44:28 -0800

    fmt

--- ci/script.sh
+++ ci/script.sh
@@ -4,11 +4,11 @@ set -e
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 echo "[url \"https://github.com/rust-lang/crates.io-index\"]" >> ~/.gitconfig
 echo "        insteadOf = https://github.com/rust-lang/crates.io-index" >> ~/.gitconfig
 
-export CARGO_NET_GIT_FETCH_WITH_CLI=true
 cargo test
 
 export PATH="$HOME/miniconda/bin:$PATH"
