7e8c7598765c7e7b98b84747d37f6c43e1ad4fd6
Richard Huang <rhuang@nextdoor.com>
2022-02-09 16:32:34 -0800

    modify gitconfig in deploy/sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,9 @@ set -e
 export PATH="$HOME/miniconda/bin:$PATH"
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
+echo "[url \"https://github.com/rust-lang/crates.io-index\"]" >> ~/.gitconfig
+echo "        insteadOf = https://github.com/rust-lang/crates.io-index" >> ~/.gitconfig
+
 cd recipe
 
 conda build .
