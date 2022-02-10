11f18c2a4e4a525aac9ddf20040905d4107127b2
Richard Huang <rhuang@nextdoor.com>
2022-02-10 09:19:22 -0800

    debug ssh and flaky builds (#27)
    * debug ssh
    
    * ssh-add
    
    * modify gitconfig
    
    * Cleanup
    
    * modify gitconfig in deploy/sh
    
    * fmt
    
    * require unit test for deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,8 +3,13 @@
 set -e
 
 export PATH="$HOME/miniconda/bin:$PATH"
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
+
+echo "[url \"https://github.com/rust-lang/crates.io-index\"]" >> ~/.gitconfig
+echo "        insteadOf = https://github.com/rust-lang/crates.io-index" >> ~/.gitconfig
 
 cd recipe
+
 conda build .
 conda build . --output | xargs anaconda -t $ANACONDA_ORG_TOKEN upload
 
